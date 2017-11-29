58a83413b9f841283bcb5d40688cfc11b70d0c62
adriaferre <adria.ferre.97@gmail.com>
2017-11-29 12:47:15 +0100

    Add jwt secret in config.js. THIS IS NOT SECURE

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    //Would be a good practice to have this variable stored in env variables
+    // instead of having hardcoded in a config file
+    JWT_SECRET: 'jwtsecret'
+};
