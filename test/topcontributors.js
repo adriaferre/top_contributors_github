1234d8a73d6f51dc9fd9db5e265940dbb9548fa3
adriaferre <adria.ferre.97@gmail.com>
2017-11-29 12:48:11 +0100

    Add tests to check if the user is sending and auth token and refactor of the previous test to send an auth token

--- test/topcontributors.js
+++ test/topcontributors.js
@@ -1,8 +1,10 @@
 const chai = require('chai');
 const chaiHttp = require('chai-http');
 const nock = require('nock');
+const jwt = require('jsonwebtoken');
 
 const server = require('../index');
+const config = require('../config');
 const should = chai.should();
 const expect = chai.expect;
 const assert = chai.assert;
@@ -16,16 +18,61 @@ describe('GET /topcontributors', () => {
         return itemsArray;
     };
 
+    const token = jwt.sign({sub: 'userId', exp: 9497619047}, config.JWT_SECRET);
+    const tokenExpired = jwt.sign({sub: 'userId', exp: 0}, config.JWT_SECRET);
+
     beforeEach(done => {
         nock.cleanAll();
         done();
     })
 
-    it('should not let me get the contributors list if no city is provided', (done) => {
-        const mock = nock('https://api.github.com').get(/search\/users/).reply(200, { items: generateFakeArray(30)});
-        
+    it('should not let me get the contributors list without auth token', (done) => {
         chai.request(server)
         .get('/contributors')
+        .end((err, res) => {
+            res.should.have.status(401);
+            res.should.be.json;
+            res.body.should.be.an('object');
+            res.body.should.have.property('message');
+            res.body.message.should.be.equal('No authorization token was found');
+            done();
+        })
+    });
+
+    it('should not let me get the contributors list with an invalid auth token', (done) => {
+        chai.request(server)
+        .get('/contributors')
+        .set('authorization', 'Bearer invalid_token_here')
+        .end((err, res) => {
+            res.should.have.status(401);
+            res.should.be.json;
+            res.body.should.be.an('object');
+            res.body.should.have.property('message');
+            res.body.message.should.be.equal('jwt malformed');
+            done();
+        })
+    });
+
+    it('should not let me get the contributors list with an expired auth token', (done) => {
+        chai.request(server)
+        .get('/contributors')
+        .set('authorization', `Bearer ${tokenExpired}`)
+        .end((err, res) => {
+            res.should.have.status(401);
+            res.should.be.json;
+            res.body.should.be.an('object');
+            res.body.should.have.property('message');
+            res.body.message.should.be.equal('jwt expired');
+            done();
+        })
+    });
+
+    it('should not let me get the contributors list if no city is provided', (done) => {
+        const mock = nock('https://api.github.com').get(/search\/users/).reply(200, { items: generateFakeArray(30)});
+
+        chai.request(server)
+        .get('/contributors')
+        .set('authorization', `Bearer ${token}`)
         .end((err, res) => {
             res.should.have.status(400);
             res.should.be.json;
@@ -42,6 +89,7 @@ describe('GET /topcontributors', () => {
 
         chai.request(server)
         .get('/contributors?city=barcelona&top=not_valid_limit')
+        .set('authorization', `Bearer ${token}`)
         .end((err, res) => {
             res.should.have.status(400);
             res.should.be.json;
@@ -58,6 +106,7 @@ describe('GET /topcontributors', () => {
 
         chai.request(server)
         .get('/contributors?city=barcelona')
+        .set('authorization', `Bearer ${token}`)
         .end((err, res) => {
             res.should.have.status(200);
             res.should.be.json;
@@ -73,6 +122,7 @@ describe('GET /topcontributors', () => {
 
         chai.request(server)
         .get('/contributors?city=barcelona&top=50')
+        .set('authorization', `Bearer ${token}`)
         .end((err, res) => {
             res.should.have.status(200);
             res.should.be.json;
@@ -88,6 +138,7 @@ describe('GET /topcontributors', () => {
         
         chai.request(server)
         .get('/contributors?city=barcelona&top=100')
+        .set('authorization', `Bearer ${token}`)
         .end((err, res) => {
             res.should.have.status(200);
             res.should.be.json;
@@ -103,6 +154,7 @@ describe('GET /topcontributors', () => {
 
         chai.request(server)
         .get('/contributors?city=barcelona&top=150')
+        .set('authorization', `Bearer ${token}`)
         .end((err, res) => {
             res.should.have.status(200);
             res.should.be.json;
