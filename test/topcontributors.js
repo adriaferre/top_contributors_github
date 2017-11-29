7001f59b1185e76bb40278fa9e6f8c0265c08b8e
adriaferre <adria.ferre.97@gmail.com>
2017-11-29 12:18:45 +0100

    Add mocks to mock the response from github api

--- test/topcontributors.js
+++ test/topcontributors.js
@@ -10,12 +10,20 @@ const assert = chai.assert;
 chai.use(chaiHttp);
 
 describe('GET /topcontributors', () => {
+    const generateFakeArray = (num) => {
+        let itemsArray = [];
+        for (let i = 0; i < num; i++) itemsArray.push(i);
+        return itemsArray;
+    };
+
     beforeEach(done => {
         nock.cleanAll();
         done();
     })
 
     it('should not let me get the contributors list if no city is provided', (done) => {
+        const mock = nock('https://api.github.com').get(/search\/users/).reply(200, { items: generateFakeArray(30)});
+        
         chai.request(server)
         .get('/contributors')
         .end((err, res) => {
@@ -24,11 +32,14 @@ describe('GET /topcontributors', () => {
             res.body.should.be.an('object');
             res.body.should.have.property('message');
             res.body.message.should.be.equal('city is not passed');
+            mock.isDone().should.equal(false);
             done();
         })
     });
 
     it('should not let me get the contributors list if top is not an integer', (done) => {
+        const mock = nock('https://api.github.com').get(/search\/users/).reply(200, { items: generateFakeArray(30)});
+
         chai.request(server)
         .get('/contributors?city=barcelona&top=not_valid_limit')
         .end((err, res) => {
@@ -37,11 +48,14 @@ describe('GET /topcontributors', () => {
             res.body.should.be.an('object');
             res.body.should.have.property('message');
             res.body.message.should.be.equal('top must be an integer');
+            mock.isDone().should.equal(false);
             done();
         })
     });
 
     it('should let me get the contributors list with 30 results (default)', (done) => {
+        const mock = nock('https://api.github.com').get(/search\/users/).reply(200, { items: generateFakeArray(30)});
+
         chai.request(server)
         .get('/contributors?city=barcelona')
         .end((err, res) => {
@@ -49,16 +63,20 @@ describe('GET /topcontributors', () => {
             res.should.be.json;
             res.body.should.be.an('array');
             res.body.should.have.length(30);
+            mock.isDone().should.equal(true);
             done();
         });
     });
 
     it('should let me get the top 50 contributors list', (done) => {
+        const mock = nock('https://api.github.com').get(/search\/users/).reply(200, { items: generateFakeArray(50)});
+
         chai.request(server)
         .get('/contributors?city=barcelona&top=50')
         .end((err, res) => {
             res.should.have.status(200);
             res.should.be.json;
+            mock.isDone().should.equal(true);
             res.body.should.be.an('array');
             res.body.should.have.length(50);
             done();
@@ -66,6 +84,8 @@ describe('GET /topcontributors', () => {
     });
 
     it('should let me get the top 100 contributors list', (done) => {
+        const mock = nock('https://api.github.com').get(/search\/users/).reply(200, { items: generateFakeArray(100)});
+        
         chai.request(server)
         .get('/contributors?city=barcelona&top=100')
         .end((err, res) => {
@@ -78,6 +98,9 @@ describe('GET /topcontributors', () => {
     });
 
     it('should let me get the top 150 contributors list', (done) => {
+        const mockPage1 = nock('https://api.github.com').get(/search\/users.*page=1/).reply(200, { items: generateFakeArray(100)});
+        const mockPage2 = nock('https://api.github.com').get(/search\/users.*page=2/).reply(200, { items: generateFakeArray(50)});
+
         chai.request(server)
         .get('/contributors?city=barcelona&top=150')
         .end((err, res) => {
@@ -85,6 +108,8 @@ describe('GET /topcontributors', () => {
             res.should.be.json;
             res.body.should.be.an('array');
             res.body.should.have.length(150);
+            mockPage1.isDone().should.equal(true);
+            mockPage2.isDone().should.equal(true);
             done();
         });
     });
